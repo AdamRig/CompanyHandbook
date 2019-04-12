f8ef0d47ccc8c141bd6ac4d8af9a8c7454799ebd
Adam Righeibi <adam-rig@hotmail.com>
2019-04-12 20:33:22 +0100

    Examples class finished

--- src/coding/standards/Examples.java
+++ src/coding/standards/Examples.java
@@ -1,5 +1,44 @@
+//package name
 package coding.standards;
 
+// import example
+import java.util.ArrayList;
+
+//class name
 public class Examples {
+	
+	//declare local variables
+	private static int a = 1;
+	private static int b = 2;
+	private static int c = 3;
+
+	private static ArrayList<Integer> myList = new ArrayList<>(3);
+	
+	public static void main(String[] args) {
+
+		// begin if statement
+		if(myList != null) {
+			
+			displayList();
+			
+		} 
+		else {
+			
+			System.out.println("The list is empty");
+		}
+
+       
+    }
+
+	// method
+	private static void displayList() {
+		
+		myList.add(a);
+        myList.add(b);
+        myList.add(c);
+
+        System.out.println(myList);
+		
+	}
 
 }
